35bdc468ff24933655459abd4b67d9ecf5bd7123
pun11 <pun11@users.noreply.github.com>
2016-02-22 17:37:59 +0100

    Uploaded correct cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming Assignment 2
+## Calculating an inverse utilizing cached values
 
-## Write a short comment describing this function
+## Create a cache of a matrix for easier and faster computations
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  M <- NULL
+  set <- function(y) {
+    x <<- y
+    M <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(solve) M <<- solve
+  getinv <- function() M
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Solving an inverse of a given matrix. If it was already computed and cached, returns the cached value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  M <- x$getinv()
+  if (!is.null(M)) {
+    message("Getting cached data")
+    return(M)
+  }
+  data <- x$get()
+  M <- solve(data, ...)
+  x$setinv(M)
+  M
 }
